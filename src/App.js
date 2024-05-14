98745160a8ce51acb9f4cc22c5440a6af2da351e
AnhThu <baothu8203@gmail.com>
2024-05-15 01:11:20 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const name = 'Thu';
+  const age = 20;
+  const isMarried = true;
+  const x = [1, 2, 3, 4];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nguyen Bao Anh Thu 
+          Nguyen Bao Anh Thu 123
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Xin chao {name} - {age}
+        </p>
+        
       </header>
     </div>
   );
